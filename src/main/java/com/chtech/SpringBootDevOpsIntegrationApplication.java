7f5f7ef441304b4125af78c1db167f119680297e
ijazgh <ijazchoudary@hotmail.com>
2025-02-19 14:54:36 -0500

    first commit

---
+++ src/main/java/com/chtech/SpringBootDevOpsIntegrationApplication.java
@@ -0,0 +1,35 @@
+package com.chtech;
+
+import jakarta.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDevOpsIntegrationApplication {
+
+	public static Logger logger = 	LoggerFactory.getLogger(SpringBootDevOpsIntegrationApplication.class);
+	
+	@GetMapping("/get")
+	
+	public String getMessage() {
+		
+		return "Hello, Welcome to the spring";
+	}
+
+	@PostConstruct
+	public void init(){
+		System.out.println("Welome to SpringbootDevopsIntegration  Application!");
+		logger.info("SpringbootDevopsIntegration Application started.....");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDevOpsIntegrationApplication.class, args);
+	}
+
+}
